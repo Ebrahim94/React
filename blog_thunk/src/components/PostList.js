4cbac98d23ecdb67c0b99e46263cde6baf798218
Anonymous <ebrahim994a@gmail.com>
2019-11-20 15:32:49 -0500

    completed PostList component

--- blog_thunk/src/components/PostList.js
+++ blog_thunk/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
+import UserHeader from './UserHeader'
 
 class PostList extends React.Component {
 
@@ -8,11 +9,33 @@ class PostList extends React.Component {
         this.props.fetchPosts()
     }
 
+    renderList(){
+        return this.props.posts.map(post =>{
+            return (
+                <div className = "item" key={post.id}>
+                    <i className = "lard middle aligned icon user"/>
+                    <div className = "content">
+                        <div className = "description">
+                            <h1>{post.title}</h1>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                    <UserHeader id = {post.userId} />
+                </div>
+            )
+        })
+    }
+
+
     render() {
         return (
-            <div>postlist</div>
+            <div className = "ui relaxed divided list">{this.renderList()}</div>
         )
     }
 }
 
-export default connect(null,{fetchPosts: fetchPosts})(PostList)
+const mapStateToProps = (state) =>{
+    return {posts: state.posts}
+}
+
+export default connect(mapStateToProps,{fetchPosts: fetchPosts})(PostList)
--- blog_thunk/src/reducers/index.js
+++ blog_thunk/src/reducers/index.js
@@ -3,4 +3,4 @@ import postsReducer from './postsReducer'
 
 
 
-export default combineReducers({post : postsReducer})
+export default combineReducers({posts : postsReducer})
