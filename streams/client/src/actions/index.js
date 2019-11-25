e06cc17567f36e7df8383d84795bf8dc75c56fa0
Anonymous <ebrahim994a@gmail.com>
2019-11-25 14:17:08 -0500

    added react form

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,13 +1,15 @@
-export const signOut = () => {
+import {SIGN_IN, SIGN_OUT} from './types'
+
+export const signIn = (userId) => {
     return {
-        type:'SIGN_IN',
-        
+        type: SIGN_IN,
+        payload: userId
     }
 }
 
-export const signIn = () => {
+export const signOut = () => {
     return {
-        type:'SIGN_OUT',
+        type: SIGN_OUT
         
     }
 }
--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux'
+import authReducer from './authReducer'
+import {reducer as formReducer} from 'redux-form'
 
 
 export default combineReducers({
-    replaceMe: () => 5
+    auth: authReducer,
+    form: formReducer
 })
