ae127310d015ff899490f179e8a7e0de12ad4755
Anonymous <ebrahim994a@gmail.com>
2019-12-01 20:51:38 -0500

    changes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -8,10 +8,6 @@ import StreamShow from './streams/StreamShow'
 import Header from './Header'
 import history from '../history'
 
-
-
-
-
 const App = () => {
 
     return (
@@ -22,7 +18,7 @@ const App = () => {
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
                     <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
                     <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </Router>
--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import Modal from '../Modal'
+import history from '../../history'
 
-const StreamDelete  = () =>{
-    return (<div>
-        StreamDelete
-        <Modal />
-    </div>)
+class StreamDelete extends React.Component {
+
+    componentDidMount(){
+        console.log(this.props.match.params.id)
+    }
+
+    actions(){
+        return (
+        <React.Fragment>
+        <button className="ui negative button">Delete</button>
+        <button className="ui button">cancel</button>
+        </React.Fragment>
+    ) 
+}
+    
+    render(){
+
+        return (<div>
+            StreamDelete
+            <Modal 
+            title = 'Delete Stream'
+            content= 'Are you sure you would like to delete this stream?'
+            button = {this.actions()}
+            onDismiss = {() => history.push('/')}
+            />
+            
+        </div>)
+    }
+    
 }
 
 export default StreamDelete
