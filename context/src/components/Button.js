3a93284431a07b6fa24a3a7889f712df3721ff47
Anonymous <ebrahim994a@gmail.com>
2019-12-05 12:08:40 -0500

    React context

---
+++ context/src/components/Button.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div>
+                <ColorContext.Consumer>
+                    {(color) =>
+                        <button className={`ui button ${color}`} >
+                            <LanguageContext.Consumer>
+                                {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+                            </LanguageContext.Consumer>
+                        </button>
+                    }
+                </ColorContext.Consumer>
+            </div>
+        )
+    }
+}
+
+export default Button
